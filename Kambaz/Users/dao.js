88b85ef5d58a2aab788aaccb4c6918ec7d75623c
Misha Tabatabai <mishat2021@gmail.com>
2025-04-10 16:28:08 -0400

    a6 again

--- Kambaz/Users/dao.js
+++ Kambaz/Users/dao.js
@@ -1,7 +1,11 @@
 import model from "./model.js";
+import { v4 as uuidv4 } from "uuid";
 
 export const
-    createUser = (user) => { };
+    createUser = (user) => {
+        const newUser = { ...user, _id: uuidv4() };
+        return model.create(newUser);
+    };
 export const
     findAllUsers = () => model.find();
 export const
